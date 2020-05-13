135e4b7e7f3c911700f84ca56815891b722d5906
michielvandijk <michiel.vandijk@wur.nl>
2020-05-14 00:09:28 +0200

    Correcting errors

--- R/view_panel.R
+++ R/view_panel.R
@@ -10,12 +10,13 @@
 #'  one of the SPAMc four letter crop codes.
 #'@param var Character. The variable to be plotted. `var` has to be physical
 #'  area (`"pa"`) or harvested area (`"ha"`).
-#'@param param #'@inheritParams create_spam_folders
+#'@param param
+#'@inheritParams create_spam_folders
 #'@param viewer Logical. Should the default web browers be used to show the
 #'  maps? `FALSE` will show the maps in the RStudio viewer pane.
 #'@param polygon Logical; should the country polygon be overlayed?
 #'
-#'#'@examples
+#'@examples
 #'\dontrun{
 #'view_panel(crop = "maiz", var = "ha", viewer = FALSE, polygon = FALSE)
 #'}
